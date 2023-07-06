2455c4716e9a7c5860c11fb05089ddfabcf9cda6
Nate Shoffner <nate.shoffner@gmail.com>
2023-07-06 05:09:39 -0400

    Unit eq

--- src/lcwc/unit.py
+++ src/lcwc/unit.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(eq=False)
 class Unit:
 
     """Represents a unit responding to an incident"""
 
     """ The name of the unit """
     name: str
+
+    def __eq__(self, other):
+        if not isinstance(other, Unit):
+            return NotImplemented
+        return self.name == other.name
