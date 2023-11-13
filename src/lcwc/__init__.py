9f0296a17a4dbc11fee2c585dddda8b0581158c4
Nate Shoffner <nate.shoffner@gmail.com>
2023-11-13 02:09:27 -0500

    Package info

--- src/lcwc/__init__.py
+++ src/lcwc/__init__.py
@@ -1,3 +1,9 @@
+__title__ = 'lcwc'
+__author__ = 'Nate Shoffner'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023-present Nate Shoffner'
+__version__ = '0.9.3'
+
 from .agencies.agency import Agency
 from .agencies.agencyclient import AgencyClient
 from .category import IncidentCategory
