47adb3084bdee574f9e723b4e312ebc2bff2bdfc
Nate Shoffner <nate.shoffner@gmail.com>
2026-08-11 03:26:04 -0400

    refactor(agencies): remove broken get_unit_agency

--- src/lcwc/agencies/agencyresolver.py
+++ src/lcwc/agencies/agencyresolver.py
@@ -1,10 +1,9 @@
-import re
+from typing import Optional
+
 from lcwc.agencies import ALL_KNOWN_AGENCIES
 from lcwc.agencies.agency import Agency
 from lcwc.agencies.exceptions import OutOfCountyException, PendingUnitException
 from lcwc.category import IncidentCategory
-from lcwc.incident import Incident
-from lcwc.unit import Unit
 
 
 class AgencyResolver:
@@ -21,7 +20,9 @@ class AgencyResolver:
     def remove_agency(self, agency: Agency):
         self.agencies.remove(agency)
 
-    def get_agency(self, station_id: str, category: IncidentCategory) -> Agency:
+    def get_agency(
+        self, station_id: str, category: IncidentCategory
+    ) -> Optional[Agency]:
         """Attempts to find the agency associated with the given station id and category within the list of agencies provided"""
         for agency in self.agencies:
             if agency.station_number == station_id and agency.category == category:
@@ -32,11 +33,3 @@ class AgencyResolver:
 
     def get_all_agencies(self) -> list[Agency]:
         return self.agencies
-
-    def get_unit_agency(self, unit: Unit, category: IncidentCategory) -> Agency:
-        """Attempts to find the agency associated with the given unit and category within the list of agencies provided"""
-
-        if unit.is_shorthand():
-            return self.__unit_short_name_to_agency(unit, category)
-        else:
-            return self.__unit_name_to_agency(unit, category)
